ed216d12d53bd04e21d96f0f105774d4206b954b
Karina <aiwpront@gmail.com>
2021-11-06 20:25:11 +0400

    updated DragAndDropTest.java

--- src/test/java/com/github/jjfhj/DragAndDropTest.java
+++ src/test/java/com/github/jjfhj/DragAndDropTest.java
@@ -1,4 +1,27 @@
 package com.github.jjfhj;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class DragAndDropTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void dragAndDropProgrammingWithSelenideActions() {
+        //Открыть страницу https://the-internet.herokuapp.com/drag_and_drop
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        //Перенести прямоугольник А на место В
+        $("#column-a").dragAndDropTo($("#column-b"));
+        //Проверить, что прямоугольники действительно поменялись
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
 }
