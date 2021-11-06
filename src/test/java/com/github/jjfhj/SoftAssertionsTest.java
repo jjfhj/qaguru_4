5031d0ecdffc3da9f3f240520379f285b0ce0085
Karina <aiwpront@gmail.com>
2021-11-06 20:09:47 +0400

    updated SoftAssertionsTest.java

--- src/test/java/com/github/jjfhj/SoftAssertionsTest.java
+++ src/test/java/com/github/jjfhj/SoftAssertionsTest.java
@@ -1,4 +1,27 @@
 package com.github.jjfhj;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SoftAssertionsTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void checkingJUnit5CodeOnTheSoftAssertionsPage() {
+        //Открыть страницу Selenide в Github
+        open("https://github.com/selenide/selenide");
+        //Перейти в раздел Wiki проекта
+        $("#wiki-tab").click();
+        //Проверить наличие страницы SoftAssertions в списке страниц (Pages) и открыть эту страницу
+        $$("#wiki-body a").findBy(text("Soft assertions")).click();
+        //Проверить наличие примера кода для JUnit5 на странице SoftAssertions
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
 }
